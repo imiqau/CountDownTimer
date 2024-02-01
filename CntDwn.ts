27e87e562c2911c7181283ade52ff2f56e5711fb
imiqau <153656435+imiqau@users.noreply.github.com>
2024-02-02 00:47:05 +0500

    Add files via upload

---
+++ CntDwn.ts
@@ -0,0 +1,27 @@
+import inquirer from 'inquirer'
+
+let date_ob = new Date();
+
+
+let user = await inquirer.prompt(
+    {
+        type:'input',
+        name:'cntr',
+        message:'Enter Count down in seconds: '
+    }
+)
+/*setTimeout(function(){
+    console.log("Executed after 1 second");
+}, 1000);*/
+var wait = (ms:number) => {
+    const start = Date.now();
+    let now = start;
+    while (now - start < ms) {
+      now = Date.now();
+    }
+}
+for (let index = user.cntr; index >= 0; index--) {
+    console.log(index);
+    wait(1000);
+    console.clear();
+}
